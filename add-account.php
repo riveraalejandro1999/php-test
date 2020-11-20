9e6888032826e8cca3dfd2de6ffb94a98ef97673
Donali <00084417@uca.edu.sv>
2020-11-19 22:26:32 -0600

    Funcionando agregar cuenta

--- add-account.php
+++ add-account.php
@@ -3,12 +3,15 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$accountNumber = "";
-$money = 0.0;
+$accountNumber  ="";
+$money = 0;
 $accountNumberErr = $moneyErr = "";
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
     // Validate name
     $inputAccountNumber = trim($_POST["accountNumber"]);
     if(empty($inputAccountNumber)){
@@ -17,21 +20,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $accountNumber = $inputAccountNumber;
     }
     
-    // Validate address
+    // Validate salary
     $inputMoney = trim($_POST["money"]);
     if(empty($inputMoney)){
-        $moneyErr = "Please enter money.";     
-    } elseif($inputMoney<0){ 
-        $moneyErr = "Please enter a positive number";
+        $moneyErr = "Please enter the amount.";     
+    } elseif(!ctype_digit($inputMoney)){
+        $moneyErr = "Please enter a positive value.";
+    } else{
+        $money = $inputMoney;
     }
-    else  $money = $inputMoney;
-    
     
     // Check input errors before inserting in database
-    //&& empty($salary_err)
-    if(empty($accountNumberErr) && empty($moneyErr) ){
-        // Prepare an insert statement
-        $sql = "INSERT INTO cuentas (numero_cuenta, monto) VALUES (?, ?)";
+    if(empty($moneyErr) && empty($address_err) && empty($salary_err)){
+        // Prepare an update statement
+        $sql = "insert into cuentas (numero_cuenta,monto) values (?,?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -43,9 +45,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Records created successfully. Redirect to landing page
-                header("location: index.php");
-                exit();
+                // Records updated successfully. Redirect to landing page
+                $recentlyInserted = mysqli_stmt_insert_id($stmt);
+                $sql = "insert into cliente_cuenta (cliente_id,cuenta_id) values (?,?)";
+                if($stmt = mysqli_prepare($link, $sql)){
+                    mysqli_stmt_bind_param($stmt, "ii", $paramClientId, $paramAccountNumber);
+                    $paramClientId = trim($_GET["id"]);
+                    $paramAccountNumber = $recentlyInserted;
+                    if(mysqli_stmt_execute($stmt)){
+                        header("location: index.php");
+                        exit();
+                    }
+
+                }
+
+
             } else{
                 echo "Something went wrong. Please try again later.";
             }
@@ -57,6 +71,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Close connection
     mysqli_close($link);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
 }
 ?>
 
@@ -65,7 +89,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <head>
     <meta charset="UTF-8">
-    <title>Create Record</title>
+    <title>Update Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
     .wrapper {
@@ -81,23 +105,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h2>Create Record</h2>
+                        <h2>Update Record</h2>
                     </div>
-                    <p>Please fill this form and submit to add account number to client</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <p>Please edit the input values and submit to update the record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
                         <div class="form-group <?php echo (!empty($accountNumberErr)) ? 'has-error' : ''; ?>">
-                            <label>Account number</label>
+                            <label>Account Number</label>
                             <input type="text" name="accountNumber" class="form-control"
                                 value="<?php echo $accountNumber; ?>">
                             <span class="help-block"><?php echo $accountNumberErr;?></span>
                         </div>
                         <div class="form-group <?php echo (!empty($moneyErr)) ? 'has-error' : ''; ?>">
                             <label>Money</label>
-                            <input type="text" name="money" class="form-control" value="<?php echo $money; ?>">
+                            <input type="text" name="money" class="form-control" value=<?php echo trim($money); ?>>
 
+                            </input>
                             <span class="help-block"><?php echo $moneyErr;?></span>
                         </div>
-
+                        <input type="hidden" name="id" value="<?php echo $id; ?>" />
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
                     </form>
